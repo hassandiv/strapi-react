57450741e3133180455d2b5b19277bc8c20bb29a
hassan <hassanhafez86@gmail.com>
2021-10-27 11:02:43 +0100

    update cms core files

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a4070c8dfdd29a37767f420016210b01'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '072d95fb-3b8b-455b-adfe-22459bbf326c'
+};
